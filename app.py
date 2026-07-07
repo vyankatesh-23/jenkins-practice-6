e264ef620351477d9ceeff30f6d5b8c0166274bb
root <root@Vyankatesh.localdomain>
2026-07-07 15:14:54 +0530

    change app.py

--- app.py
+++ app.py
@@ -9,3 +9,10 @@ def multi(a, b):
 
 def divide(a, b):
     return a / b
+
+if __name__ == "__main__":
+    print(f"Addition: {add(10, 5)}")
+    print(f"subtraction: {sub(10, 5)}")
+    print(f"multiplication: {multi(10, 5)}")
+    print(f"division: {divide(10, 5)}")
+
